d7c0ba5fe6ff42c8a1d349c69bb1b9ebd9abf21c
alexandra.volushkova <alexandra.volushkova@ax-team.com>
2023-09-26 12:54:15 +0300

    add copy right

--- src/main/java/org/axenix/axenapi/model/kafka/remote/RemoteMethodVariableMetadata.java
+++ src/main/java/org/axenix/axenapi/model/kafka/remote/RemoteMethodVariableMetadata.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright [yyyy] [name of copyright owner]
+ * Copyright (C) 2023 Axenix Innovations LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 
 
 /*
- * Copyright [yyyy] [name of copyright owner]
+ * Copyright (C) 2023 Axenix Innovations LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
