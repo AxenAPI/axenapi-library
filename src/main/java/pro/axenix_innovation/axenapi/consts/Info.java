5ce8c75008efc3aceece82a9a546c4796f7b479e
alexandra.volushkova <alexandra.volushkova@ax-team.com>
2023-11-09 18:30:01 +0300

    change group and package

--- src/main/java/pro/axenix_innovation/axenapi/consts/Info.java
+++ src/main/java/pro/axenix_innovation/axenapi/consts/Info.java
@@ -20,7 +20,7 @@ package pro.axenix_innovation.axenapi.consts;
 public class Info {
     private Info() {}
 
-    public static final String BASE_PACKAGE = "org.axenix.axenapi";
+    public static final String BASE_PACKAGE = "pro.axenix_innovation.axenapi";
     public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".controller";
     public static final String DTO_PACKAGE = BASE_PACKAGE + ".dto";
 }
